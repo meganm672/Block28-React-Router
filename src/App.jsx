292bfe069e26acae1146dc9953ad18c3b395b4cc
meganm672 <meganm672@gmail.com>
2023-09-28 20:43:45 -0400

    completed the router configuration tab

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@ import { useState } from 'react'
 
 import './App.css'
 
+//import Routes and route props
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div id="container">
-      <h1>Hello React Router!</h1>
-      <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
-    </div>
+          <div id="navbar">{/* navigation here */}</div>
+          <div id="main-section">
+            <Routes>
+              <Route path="/blue" element={<h1>Blue</h1>}/>
+              <Route path="/red" element={<h1>Red</h1>} />
+            </Routes>
+          </div>
+      </div>
     </>
   )
 }
