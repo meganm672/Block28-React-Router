4238a70e79ae3ce7bcc521d9d7b19dfcef0a95ad
meganm672 <meganm672@gmail.com>
2023-09-28 22:01:49 -0400

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import './App.css'
 //import Navbar and Main from components folder
 import Navbar from './components/Navbar'
 import MainSection from './components/MainSection'
-
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div id="container">
       <Navbar />      
       <MainSection />
+      <Footer />
     </div>
   )
 }
